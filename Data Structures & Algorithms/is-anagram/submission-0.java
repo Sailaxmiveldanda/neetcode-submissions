a84aa1ba92d5ed02109655fb005c7e578a46d96d
Sailaxmi V <122695930+Sailaxmiveldanda@users.noreply.github.com>
2026-09-02 18:49:11 -0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        Map<Character,Integer> m1 = new HashMap<>();
+        Map<Character,Integer> m2 = new HashMap<>();
+
+        for(char ch : s.toCharArray()){
+            m1.put(ch, m1.getOrDefault(ch, 0)+1);
+        }
+
+        for(char ch : t.toCharArray()){
+            m2.put(ch, m2.getOrDefault(ch, 0) +1);
+        }
+        return m1.equals(m2);
+    }
+}
